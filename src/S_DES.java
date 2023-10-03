7e0907276e4c04dc7753159824b0affc9b2022db
yan3935 <145537106+yan3935@users.noreply.github.com>
2023-10-03 10:50:39 +0800

    Update S_DES.java

--- src/S_DES.java
+++ src/S_DES.java
@@ -29,33 +29,87 @@ public class S_DES {
     }*/
 
     private void IPBox(char[] a){
-
+        char[] result = new char[8];
+         for (int i = 0; i < 8; i++) {
+            result[i] = a[IP[i] - 1];
+        }
+        return result;
     }
 
     private void nonIPBox(char[] a){
-
+        char[] result = new char[8];
+        for (int i = 0; i < 8; i++) {
+            result[i] = a[IP[i] - 1];
+        }
+        return result;
     }
 
-    private void EPBox(){
-
+    private void EPBox(char[] a){
+        char[] result = new char[8];
+        for (int i = 0; i < 8; i++) {
+            result[i] = a[EP[i] - 1];
+        }
+        return result;
     }
 
-    private void SPBox(){
+    private void SPBox(char[] a){
+        char[] result = new char[4];
+        // Split the 8-bit input into two 4-bit parts
+        char[] left = Arrays.copyOfRange(input, 0, 4);
+        char[] right = Arrays.copyOfRange(input, 4, 8);
 
+        // Perform S-box substitution for S0 and S1
+        int s0Row = Integer.parseInt("" + left[0] + left[3], 2);
+        int s0Col = Integer.parseInt("" + left[1] + left[2], 2);
+        int s1Row = Integer.parseInt("" + right[0] + right[3], 2);
+        int s1Col = Integer.parseInt("" + right[1] + right[2], 2);
+
+        int s0Output = S0[s0Row][s0Col];
+        int s1Output = S1[s1Row][s1Col];
+
+        // Convert S-box outputs to binary strings
+        String s0OutputBinary = String.format("%2s", Integer.toBinaryString(s0Output)).replace(' ', '0');
+        String s1OutputBinary = String.format("%2s", Integer.toBinaryString(s1Output)).replace(' ', '0');
+
+        // Concatenate the S-box outputs
+        String spResult = s0OutputBinary + s1OutputBinary;
+
+        // Convert the result to a character array
+        for (int i = 0; i < 4; i++) {
+            result[i] = spResult.charAt(i);
+        }
+
+        return result;
     }
 
-    private  void SBox1(){
-
+    private char[] SBox1(char[] a) {
+        return SPBox(a);
     }
 
-    private void SBox2(){
-
+    private char[] SBox2(char[] a) {
+        return SPBox(a);
     }
 
-    private void Xor(){
-
+    private char[] Xor(char[] a, char[] b) {
+        char[] result = new char[a.length];
+        for (int i = 0; i < a.length; i++) {
+            result[i] = (a[i] == b[i]) ? '0' : '1';
+        }
+        return result;
     }
 
+    public static void main(String[] args) {
+        
+        // Example usage
+        char[] plaintext = "11010110".toCharArray();
+        char[][] keys = new char[16][8];
+        for (int i = 0; i < 16; i++) {
+            keys[i] = "10111011".toCharArray();  // Replace with your actual key values
+        }
+
+        S_DES desScheduler = new S_DES(plaintext, keys);
+
+    }
 
 
 }
