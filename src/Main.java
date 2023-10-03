6a5f113b44223dd2f3521f01a99cffb2a838dfeb
yan3935 <145537106+yan3935@users.noreply.github.com>
2023-10-03 11:25:20 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 package src;
 
-public class Main {
-}
+public class Main(String[] args) {
+      
+        // 示例
+        char[] plaintext = "11010110".toCharArray();
+        char[][] keys = new char[16][8];
+        for (int i = 0; i < 16; i++) {
+            keys[i] = "10111011".toCharArray();  // 替换为实际密码
+        }
+
+        S_DES desScheduler = new S_DES(plaintext, keys);
+  
+    }
+} 
